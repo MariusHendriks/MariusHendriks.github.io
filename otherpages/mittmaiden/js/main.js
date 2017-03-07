b443badef63c9d7879b5669be57a772d1f1d5f59
Mark Hendriks <mark.hendriks3@rocdeleijgraaf.nl>
2017-03-07 12:51:54 +0000

    bugfixes

--- otherpages/mittmaiden/js/main.js
+++ otherpages/mittmaiden/js/main.js
@@ -79,5 +79,121 @@ function main() {
 }());
 
 
+// start video fade in
+var e = document.getElementById("bgv");
+e.style.opacity = 0;
+
+var vid = document.getElementById("bgv");
+vid.oncanplaythrough = function() {
+	var e = document.getElementById('bgv');
+    fade(e);
+};
+vid.addEventListener('ended', function () {
+  var op2 = 1;
+	var timer2 = setInterval(function() {
+        if (op2 <= 0) {
+        	clearInterval(timer2);
+        }
+        vid.style.opacity = op2;
+        vid.style.filter = 'alpha(opacity=' + op2 * 100 + ")";
+        op2 -= op2 * 0.1 || 0.1;
+    }, 50);
+});
+//start video fade out
+function fade(element) {
+    var op = 0;
+    var timer = setInterval(function() {
+        if (op >= 1) {
+        	clearInterval(timer);
+        	vid.play();
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1 || 0.1;
+    }, 50);
+}
+//switch video
+vid.addEventListener("ended", hideVideo, false);
+aftervideo.style.display='none';
+aftervideo.style.opacity = 0; 
+function hideVideo() {
+	setTimeout(
+		function(){
+			var vid=document.getElementById('bgv');
+			var aftervideo=document.getElementById('aftervideo');
+			vid.removeEventListener("ended", hideVideo, false);
+			vid.style.display='none'; 
+
+			
+			fade(aftervideo);
+			aftervideo.style.display='block';
+			
+		}, 3000);
+	}
+
+//fade in image
+
+	
+$(document).ready(function(){
+    $('.awesome-tooltip').tooltip({
+        placement: 'left'
+    });   
+
+    $(window).bind('scroll',function(e){
+      dotnavigation();
+    });
+    
+    function dotnavigation(){
+             
+        var numSections = $('section').length;
+        
+        $('#dot-nav li a').removeClass('active').parent('li').removeClass('active');     
+        $('section').each(function(i,item){
+          var ele = $(item), nextTop;
+          
+          console.log(ele.next().html());
+          
+          if (typeof ele.next().offset() != "undefined") {
+            nextTop = ele.next().offset().top;
+          }
+          else {
+            nextTop = $(document).height();
+          }
+          
+          if (ele.offset() !== null) {
+            thisTop = ele.offset().top - ((nextTop - ele.offset().top) / numSections);
+          }
+          else {
+            thisTop = 0;
+          }
+          
+          var docTop = $(document).scrollTop();
+          
+          if(docTop >= thisTop && (docTop < nextTop)){
+            $('#dot-nav li').eq(i).addClass('active');
+          }
+        });   
+    }
+
+    /* get clicks working */
+    $('#dot-nav li').click(function(){
+      
+        var id = $(this).find('a').attr("href"),
+          posi,
+          ele,
+          padding = 0;
+      
+        ele = $(id);
+        posi = ($(ele).offset()||0).top - padding;
+      
+        $('html, body').animate({scrollTop:posi}, 'slow');
+      
+        return false;
+    });
+
+/* end dot nav */
+});
+
+
 }
 main();
